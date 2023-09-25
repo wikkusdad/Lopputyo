53d2223f068775b387c8a28d46c159f3711faf3b
wikkusdad <roope.pajarinen@gmail.com>
2023-09-25 16:19:38 +0300

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -10,55 +10,84 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    //korkeus ja leveys muuttujat
+    // Korkeus ja leveys muuttujat
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
 
     return Scaffold(
       body: Column(
         children: [
-          //kuva container
+          // Kuva container
           Container(
             width: w,
             height: h * 0.3,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage("assets/images/Sneakers.png"),
-                  fit: BoxFit.cover),
+                image: AssetImage("assets/images/Sneakers.png"),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           Container(
-              //teksti container
-              width: w,
-              margin: const EdgeInsets.only(left: 22, right: 22),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hei!',
-                    style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Kirjaudu sisään',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                  ),
-                  SizedBox(
-                    height: 30, // tilaa ennen textinputteja
-                  ),
-                  TextField(
+            // Teksti container
+            width: w,
+            margin: const EdgeInsets.only(left: 22, right: 22),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Hei!',
+                  style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  'Kirjaudu sisään',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+                const SizedBox(
+                  height: 30, // Tilaa ennen textinputteja
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextField(
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                     ),
                   ),
-                  SizedBox(
-                      height: 14.0), //lisätään marginaalia laatikoiden väliin
-                  TextField(
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextField(
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                     ),
                   ),
-                ],
-              ))
+                ),
+                SizedBox(height: 25), // Marginaali
+                Row(
+                  children: [
+                    Expanded(child: Container()),
+                    const Text(
+                      'Unohditko salasanan',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 45, vertical: 20),
+                    ),
+                    child: Text('Kirjaudu'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
